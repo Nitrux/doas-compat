609a0c732a0147598a0e90584a539715694f1be2
UriHerrera <hello@uriherrera.com>
2022-01-05 23:04:50 -0600

    create pkg

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# doas-compat
-sudo compatibility package.
+# Doas compat [![Build Status](https://travis-ci.org/Nitrux/doas-compat.svg?branch=master)](https://travis-ci.org/Nitrux/doas-compat)
+
+Package to solve dependency of sudo.
+
+# Issues
+If you find problems with the contents of this repository please create an issue.
+
+©2022 Nitrux Latinoamericana S.C.
---
+++ travis-build.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+apt -qq update
+apt -qq -yy install equivs curl git
+
+deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
+git_commit=$(git rev-parse --short HEAD)
+
+> configuration printf "%s\n" \
+	"Section: misc" \
+	"Priority: optional" \
+	"Homepage: https://nxos.org" \
+	"Package: doas-compat" \
+	"Version: 0.0.1-$git_commit" \
+	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
+	"Depends: $deps" \
+	"Provides: sudo" \
+	"Architecture: amd64" \
+	"Description: Package to resolve dependency when removing sudo."
+
+equivs-build configuration
